5d0d8d4d5b85f9628523f3cd23394edc5cc55850
Denis Romanov <zweroboi@gmail.com>
2014-05-11 00:18:05 +0400

    Generalize utility types for common node cases

--- llrb/util.go
+++ llrb/util.go
@@ -6,12 +6,42 @@ package llrb
 
 type Int int
 
+type IntItem interface {
+	Key() Int
+}
+
+func (k Int) Key() Int {
+	return k
+}
+
 func (x Int) Less(than Item) bool {
-	return x < than.(Int)
+	return x < than.(IntItem).Key()
 }
 
 type String string
 
+type StringItem interface {
+	Key() String
+}
+
+func (k String) Key() String {
+	return k
+}
+
 func (x String) Less(than Item) bool {
-	return x < than.(String)
+	return x < than.(StringItem).Key()
+}
+
+type Float64 float64
+
+type Float64Item interface {
+	Key() Float64
+}
+
+func (k Float64) Key() Float64 {
+	return k
+}
+
+func (k Float64) Less(than Item) bool {
+	return k < than.(Float64Item).Key()
 }
